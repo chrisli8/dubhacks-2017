b28e1455937fd74b7aa6730eb2dd035514c4e5ad
Benjamin Jin <Benjamin.z.jin@gmail.com>
2017-10-22 07:42:36 -0700

    adds tags to event object

--- BenNChrisMaps/Event.swift
+++ BenNChrisMaps/Event.swift
@@ -6,12 +6,14 @@ public struct User {
   public var creator: String
   public var description: String
   public var headcount: Int
+  private var tags: [String]
 
-  init(email: String, creator: String, description: String, headcount: Int) {
+  init(email: String, creator: String, description: String, headcount: Int, tags: [String]) {
     self.email = email
     self.creator = creator
     self.description = description
     self.headcount = headcount
+    self.tags = tags
   }
 
   public func getEmail() -> String {
@@ -30,12 +32,21 @@ public struct User {
     return self.headcount
   }
 
+  public func getTags() -> [String] {
+    return self.tags
+  }
+
   public func toAnyObject() -> {
+    var tagString = nil
+    for tag in tags {
+      tagString += "\"tag\": " + tag + ", "
+    }
     return [
       "email": self.email,
       "creator": self.creator,
       "description": self.description,
-      "headcount": self.headcount
+      "headcount": self.headcount,
+      "tags":
     ]
   }
 }
